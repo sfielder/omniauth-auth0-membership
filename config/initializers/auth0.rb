1470d233e951dfa5364d660555832d3b41fc9d50
Scott Fielder <scottkfielder@gmail.com>
2016-01-12 13:02:06 -0500

    update initializer

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    ENV["AUTH0_CLIENT_ID"],
-    ENV["AUTH0_CLIENT_SECRET"],
-    ENV["AUTH0_DOMAIN"],
+    'FM6eEBG7PsnuFbILqKZAVe3NDWVPMLSH',
+    'uClpvJUVWMHh-jA19aLDCDsQnoJtPCOFFpqIa7RoFBrPrdDPtKRdARjmy95emg6B',
+    'sfielder.auth0.com',
     callback_path: "/auth/auth0/callback"
   )
 end
