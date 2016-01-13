60f81830334d8c09e80dbb9ef34480af56f559df
Scott Fielder <scottkfielder@gmail.com>
2016-01-13 06:29:33 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'membership/index'
-
-  get 'membership/show'
-
-  get 'membership/new'
-
-  get 'membership/create'
+  resources :membership
 
   get 'dashboard/show'
 
