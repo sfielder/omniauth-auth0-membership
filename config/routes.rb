c5fae4c4e4eb13ab284277269e0c854d85ae13da
Scott Fielder <scottkfielder@gmail.com>
2016-01-14 10:13:18 -0500

    adding sign out session destroy

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/dashboard" => "dashboard#show"
 
   get "/callback" => "auth0#callback"
+  get "/logout" => "auth0#logout"
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
 
